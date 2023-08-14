1bdf8f2bc7b305bd7796526903483bd69ca8c936
Luc4sRamos <lhramosjr@outlook.com>
2023-08-14 00:04:58 -0300

    terminando página y agregando gitignore

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const PORT = process.env.PORT || 3030
+
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(3030, () =>  {
+app.listen(PORT, () =>  {
     console.log("Servidor Corriendo Mercado Liebre");
 })
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/register.html", (req, res)=>{
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.get("/login.html", (req, res)=>{
+    res.sendFile(__dirname + "/views/login.html")
 })
